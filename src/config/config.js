67ede766f81e76d5c3e7f6ecc8afb1ae81cfe972
Mathias Teran <agusmaty23@gmail.com>
2026-04-21 14:10:25 -0500

    feat: actualizar la creación del cliente Supabase para usar la clave de rol de servicio si está disponible

--- src/config/config.js
+++ src/config/config.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY)
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_ANON_KEY;
+const supabase = createClient(process.env.SUPABASE_URL, supabaseKey)
 
 export default supabase;
